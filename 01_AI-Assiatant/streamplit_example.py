89feff9b238619dd6051d41bb059dc73f32f0a4e
higogoc <selee203@naver.com>
2023-12-12 17:24:09 +0000

    streamlit

--- 01_AI-Assiatant/streamplit_example.py
+++ 01_AI-Assiatant/streamplit_example.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-
+# 제목
 st.title("My First Streamlit App")
+st.header("header")
+st.subheader("sub-header")
+
+st.text("tex")
+
+st.write("it can express text, data or plot")
